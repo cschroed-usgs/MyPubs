47e00943ad0b5a3ad342bfa415b3d3613de51bcd
David Uselmann <duselmann@compcc.com>
2014-06-14 16:00:58 -0500

    added content-type and java 7 error handling to test http server

--- pw-mypubs-ui/src/test/java/gov/cida/net/HTTP.java
+++ pw-mypubs-ui/src/test/java/gov/cida/net/HTTP.java
@@ -9,6 +9,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -19,6 +20,39 @@ public class HTTP {
 	public static final int    PORT = 8080;
 	public static final String PATH = "src/main/webapp";
 
+	public static void log (Object ... msgs) {
+		for (Object msg : msgs) {
+            System.out.print(msg);
+		}
+        System.out.println();
+	}
+	
+	
+	public static String getContentType(String path) {
+		String contentType = "text/plain";
+		if (path.endsWith(".pdf")) {
+			contentType = "application/pdf";
+		} else if (path.endsWith(".html")) {
+			contentType = "text/html";
+		} else if (path.endsWith(".css")) {
+			contentType = "text/css";
+		} else if (path.endsWith(".js")) {
+			contentType = "application/javascript";
+		} else if (path.endsWith(".json")) {
+			contentType = "application/json";
+		} else if (path.endsWith(".gif")) {
+			contentType = "image/gif";
+		} else if (path.endsWith(".jpg")) {
+			contentType = "image/jpeg";
+		} else if (path.endsWith(".png")) {
+			contentType = "image/png";
+		} else if (path.endsWith(".xml")) {
+			contentType = "application/xml";
+		}
+		return contentType;
+	}
+	
+	
 	public static void main (String...args) throws Exception {
 		InetSocketAddress address = new InetSocketAddress(PORT);
 		HttpServer server = HttpServer.create(address, 0);
@@ -27,26 +61,39 @@ public class HTTP {
 
 	        public void handle(HttpExchange exchange) throws IOException {
 	        	String path = exchange.getRequestURI().getPath();
-	        	File file = new File(PATH+path);
-	        	if ( file.exists() ) {
-	        		FileInputStream fis = new FileInputStream(file);
-		            BufferedInputStream bis = new BufferedInputStream(fis);
-		            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, file.length());
-		            System.out.print(file.length());
-		            System.out.print('\t');
-		            System.out.println(path);
-	            	OutputStream os = exchange.getResponseBody();
-	            	byte buffer[] = new byte[4096];
-	            	int len = 0;
-		            while ( (len=bis.read(buffer)) !=0 ) {
-		            	os.write(buffer,0,len);
-		            }
-		            os.close();
-		            fis.close();
-	        	} else {
-		            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+        		// java 7 try-with-resources
+	        	try ( OutputStream os = exchange.getResponseBody(); ) {
+		        	File file = new File(PATH+path);
+		        	if ( file.exists() ) {
+		        		try (   FileInputStream fis = new FileInputStream(file);
+		        				BufferedInputStream bis = new BufferedInputStream(fis);
+		        				) {
+		        			Headers headers = exchange.getResponseHeaders();
+		        			headers.add("Content-Type", getContentType(path));
+		        			
+				            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, file.length());
+				            log(file.length(),'\t',path);
+			            	byte buffer[] = new byte[4096];
+			            	int len = 0;
+				            while ( (len=bis.read(buffer)) >0 ) {
+				            	os.write(buffer,0,len);
+				            }
+		        		}
+		        	} else {
+		        		String msg = "404";
+			            log(0,'\t',path,'\t',msg);
+			            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, msg.length());
+		            	os.write( msg.getBytes() );
+		        	}
+	        	} catch (Exception e) {
+	        		e.printStackTrace();
+	        		String msg = "500 "+e.getClass().getName()+':'+e.getMessage();
+		            log(0,'\t',path,'\t',msg);
+		            exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, msg.length());
+	            	try ( OutputStream os = exchange.getResponseBody(); ) {
+	            		os.write( msg.getBytes() );
+	            	}
 	        	}
-	            exchange.close();
 	        }
 	    };
 
