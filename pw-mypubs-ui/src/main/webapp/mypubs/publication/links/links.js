f2c21f0bac982aa514c6707fac36cb338e631e53
Mary Bucknell <mbucknell@usgs.gov>
2014-07-30 12:21:25 -0500

    Used the new ListOrderingService in links.js and updated the tests.

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/links/links.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/links/links.js
@@ -1,39 +1,19 @@
 (function() {
 
 
-angular.module('pw.links',['pw.lookups'])
+angular.module('pw.links',['pw.lookups', 'pw.dataList'])
 
     .controller('linksCtrl',
-	['$scope', 'LookupFetcher', function($scope, LookupFetcher) {
+	['$scope', 'LookupFetcher', 'ListOrderingService', function($scope, LookupFetcher, ListOrderingService) {
 
-		var getEmptyLink = function(links) {
-		    var newRank;
-		    if (links.length === 0) {
-			newRank = 1;
-		    }
-		    else {
-			newRank = _.max(links, function(link) { return link.rank; }).rank + 1;
-		    }
-
-		    return {
-			id : '',
-			rank : newRank,
-			type : {},
-			url : '',
-			text : '',
-			size : '',
-			mime_type : {},
-			description : ''
-		    };
-		};
-
-		var updateRank = function(links) {
-		    var i;
-		    var result = links;
-		    for (i = 0; i < result.length; i++) {
-			result[i].rank = i + 1;
-		    }
-		    return result;
+		var EMPTY_LINK = {
+		    id : '',
+		    type : {},
+		    url : '',
+		    text : '',
+		    size : '',
+		    mime_type : {},
+		    description : ''
 		};
 
 		if (angular.isUndefined($scope.pubData.links)) {
@@ -42,27 +22,25 @@ angular.module('pw.links',['pw.lookups'])
 		$scope.pubData.links = _.sortBy($scope.pubData.links, 'rank');
 
 		// Fetch the link lookups for link type and file type
-//		LookupFetcher.promise('linktypes').then(function(response) {
-		    $scope.linkTypeOptions = [{id : 1, text: 'Text1'}];//response.data;
-//		});
-//		LookupFetcher.promise('linkfiletypes').then(function(response) {
-		    $scope.fileTypeOptions = [{id : 1, text : 'Text2'}]; //response.data;
-//		});
+		LookupFetcher.promise('linktypes').then(function(response) {
+		    $scope.linkTypeOptions = response.data; //[{id : 1, text: 'Text1'}];//response.data;
+		});
+		LookupFetcher.promise('linkfiletypes').then(function(response) {
+		    $scope.fileTypeOptions = response.data;//[{id : 1, text : 'Text2'}]; //response.data;
+		});
 
 		$scope.sortOptions = {
 		    stop : function(e, ui) {
-			//Update rank
-			$scope.pubData.links = updateRank($scope.pubData.links);
+			ListOrderingService.updateRank($scope.pubData.links);
 		    }
 		};
 
 		$scope.addNewLink = function() {
-		    $scope.pubData.links.push(getEmptyLink($scope.pubData.links));
+		    ListOrderingService.addNewObj($scope.pubData.links, EMPTY_LINK);
 		};
 
 		$scope.deleteLink = function(index) {
-		    $scope.pubData.links.splice(index, 1);
-		    $scope.pubData.links = updateRank($scope.pubData.links);
+		    ListOrderingService.deleteObj($scope.pubData.links, index);
 		};
 	}]);
 }) ();
