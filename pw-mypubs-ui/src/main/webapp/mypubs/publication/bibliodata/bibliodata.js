5c3bd6f0a33fddb6ced9da535034aebb37cc2356
Carl Schroedl <cschroedl@usgs.gov>
2014-08-05 17:51:20 -0500

    Partial restoration of functionality.

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/bibliodata/bibliodata.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/bibliodata/bibliodata.js
@@ -3,8 +3,8 @@
 
 angular.module('pw.bibliodata',['pw.dataRow','pw.fetcher', 'pw.lookups'])
     .controller('biblioCtrl', [
-        '$scope', 'LookupFetcher', 'LookupCascadeSelect2',
-        function ($scope, LookupFetcher, LookupCascadeSelect2) {
+        '$scope', 'LookupFetcher', 'LookupCascadeSelect2', '$routeParams',
+        function ($scope, LookupFetcher, LookupCascadeSelect2, $routeParams) {
 
             // This are used to control whether the change* functions are executed.
             // We don't want them to execute the first time it is fired when publication
@@ -12,57 +12,15 @@ angular.module('pw.bibliodata',['pw.dataRow','pw.fetcher', 'pw.lookups'])
             var typeInputIsInitialized = false;
             var genreInputIsInitialized = false;
 
-            $scope.type = '';
-            $scope.genre = '';
-            $scope.collectionTitle = '';
-            $scope.costCenters = [];
-            $scope.subseriesTitle = '';
-            $scope.number = '';
-            $scope.chapterNumber = '';
-            $scope.suChapterNumber = '';
-            $scope.title = '';
-            $scope.abstract = '';
-            $scope.usgsCitation = '';
-            $scope.language = '';
-            $scope.publisher = '';
-            $scope.publisherPlace = '';
-            $scope.doi = '';
-            $scope.issn = '';
-            $scope.isbn = '';
-            if (_.isEmpty($scope.pubData)){
+            if (!$routeParams.pubsid){
                 typeInputIsInitialized = true;
                 genreInputIsInitialized = true;
-            }
-            else {
-                $scope.type = $scope.pubData.type.id;
-                $scope.genre = $scope.pubData.genre.id;
-                $scope.collectionTitle = $scope.pubData['collection-title'].id;
-                angular.forEach($scope.pubData['cost-center'], function(c) {
-                    $scope.costCenters.push(c.id);
-                });
-                $scope.subseriesTitle = $scope.pubData['subseries-title'];
-                $scope.number = $scope.pubData.number;
-                $scope.chapterNumber = $scope.pubData['chapter-number'];
-                $scope.subChapterNumber = $scope.pubData['sub-chapter-number'];
-                $scope.title = $scope.pubData.title;
-                $scope.abstract = $scope.pubData.abstract;
-                $scope.usgsCitation = $scope.pubData['usgs-citation'];
-                $scope.language = $scope.pubData.language;
-                $scope.publisher = $scope.pubData.publisher;
-                $scope.publisherPlace = $scope.pubData['publisher-place'];
-                $scope.doi = $scope.pubData.DOI;
-                $scope.issn = $scope.pubData.ISSN;
-                $scope.isbn = $scope.pubData.ISBN;
-
-                // The first time the selects are initialized I do not want the change functions to execute
-                typeInputIsInitialized = false;
-                genreInputIsInitialized = false;
             }
 
             $scope.changeType = function() {
                 if (typeInputIsInitialized) {
-                    $scope.genre = '';
-                    $scope.collectionTitle = '';
+                    $scope.pubData.genre.id = '';
+                    $scope.pubData['collection-title'] = '';
                 }
                 else {
                     typeInputIsInitialized = true;
@@ -71,7 +29,7 @@ angular.module('pw.bibliodata',['pw.dataRow','pw.fetcher', 'pw.lookups'])
 
             $scope.changeGenre = function() {
                 if (genreInputIsInitialized) {
-                    $scope.collectionTitle = '';
+                    $scope.pubData['collectionTitle'] = '';
                 }
                 else {
                     genreInputIsInitialized = true;
@@ -89,10 +47,10 @@ angular.module('pw.bibliodata',['pw.dataRow','pw.fetcher', 'pw.lookups'])
 
             $scope.subtypeSelect2Options = {
                 query : function(query) {
-                    LookupCascadeSelect2.query(query, 'publicationsubtypes', {publicationtypeid : $scope.type});
+                    LookupCascadeSelect2.query(query, 'publicationsubtypes', {publicationtypeid : $scope.pubData.type.id});
                 },
                 initSelection : function(element, callback) {
-                    LookupCascadeSelect2.initSelection('publicationsubtypes', {publicationtypeid : $scope.type}, $scope.pubData.genre.id, callback);
+                    LookupCascadeSelect2.initSelection('publicationsubtypes', {publicationtypeid : $scope.pubData.type.id}, $scope.pubData.genre.id, callback);
                 },
                 placeholder : 'Select a publication subtype'
             };
