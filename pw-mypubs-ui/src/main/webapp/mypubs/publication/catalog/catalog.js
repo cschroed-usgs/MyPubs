3ffabce57b5d1e7aa6e549d2f4ff7ee5f92a702b
Jordan Walker <jiwalker@usgs.gov>
2014-08-05 17:08:44 -0500

    PUBSTWO-911
    * Switching view to use pubsData model
    * Simplifies controller to doing nothing

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/catalog/catalog.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/catalog/catalog.js
@@ -4,26 +4,6 @@
 		.controller('catalogCtrl', [
 			'$scope',
 			function($scope) {
-
-				$scope.productDescription = '';
-				$scope.numberOfPages = '';
-				$scope.pageFirst = '';
-				$scope.pageLast = '';
-				$scope.onlineOnly = 'N';
-				$scope.additionalOnlineFiles = 'N';
-				$scope.temporalStart = '';
-				$scope.temporalEnd = '';
-				$scope.notes = '';
-				if (!_.isEmpty($scope.pubData)) {
-					$scope.productDescription = $scope.pubData.productDescription;
-					$scope.numberOfPages = $scope.pubData.numberOfPages;
-					$scope.pageFirst = $scope.pubData.pageFirst;
-					$scope.pageLast = $scope.pubData.pageLast;
-					$scope.onlineOnly = $scope.pubData.onlineOnly;
-					$scope.additionalOnlineFiles = $scope.pubData.additionalOnlineFiles;
-					$scope.temporalStart = $scope.pubData.temporalStart;
-					$scope.temporalEnd = $scope.pubData.temporalEnd;
-					$scope.notes = $scope.pubData.notes;
-				}
+				// nothing to see here
 			}]);
 }) ();
