015ea7c7281c0a289721eb9cd0f6b0b5df10fea7
Jordan Walker <jiwalker@usgs.gov>
2014-08-05 15:45:00 -0500

    PUBSTWO-911
    * Restructured catalog.html to contain relevant fields
    * Hooked up controller to modified html template
    * Using modified JSON from pubData

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/catalog/catalog.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/catalog/catalog.js
@@ -1,103 +1,29 @@
 (function() {
 
+	angular.module('pw.catalog', [])
+		.controller('catalogCtrl', [
+			'$scope',
+			function($scope) {
 
-angular.module('pw.catalog',['pw.dataRow','pw.fetcher'])
-
-
-.controller('catalogCtrl', [
-'$scope', 'DataRowFieldService', 'catalogFields',
-function ($scope, DataRowFieldService, fields) {
-
-	var pubData = $scope.pubData;
-	$scope.rows = fields;
-	DataRowFieldService.fieldMapper(fields, pubData);
-
-}])
-
-
-.factory('catalogFields', function() {
-	return [
-		// {
-		// 	name   : "keywords",
-		// 	label  : "Keywords",
-		// 	rowType: "MultiAjax",
-		// 	options: [{value:"1",text:'Need-To-Make-This'},{value:"2",text:'A-Multiple-Select'},],
-		// 	placeholder:"Enter Keywords",
-		// },
-		{
-			rowType: "Gap",
-		},
-		{
-			additional:true,
-			name   : "product-description",
-			label  : "Product Description",
-			rowType: "Text",
-		},
-		{
-			rowType: "Gap",
-		},
-		{
-			name   : "page-first",
-			label  : "Start Page",
-			rowType: "Text",
-		},
-		{
-			name   : "page-last",
-			label  : "End Page",
-			rowType: "Text",
-		},
-		{
-			name   : "number-of-pages",
-			label  : "Number of Pages",
-			rowType: "Text",
-		},
-		{
-			rowType: "Gap",
-		},
-		{
-			additional:true,
-			name   : "online-only",
-			label  : "Online Only",
-			rowType: "Checkbox",
-		},
-		{
-			additional:true,
-			name   : "additional-online-files",
-			label  : "Additional Online Files",
-			rowType: "Checkbox",
-		},
-		{
-			rowType: "Gap",
-		},
-		{
-			additional:true,
-			name   : "temporal-start",
-			label  : "Temporal Start",
-			rowType: "Date",
-			elId   : "temporalStart"
-		},
-		{
-			additional:true,
-			name   : "temporal-end",
-			label  : "Temporal End",
-			rowType: "Date",
-			elId   : "temporalEnd"
-		},
-		{
-			rowType: "Gap",
-		},
-		{
-			additional:true,
-			name   : "Notes",
-			label  : "Notes",
-			rowType: "Textbox",
-		},
-		{
-			rowType: "Gap",
-		},
-
-	]
-})
-
-
-}) ()
+				$scope.productDescription = '';
+				$scope.numberOfPages = '';
+				$scope.pageFirst = '';
+				$scope.pageLast = '';
+				$scope.onlineOnly = 'N';
+				$scope.additionalOnlineFiles = 'N';
+				$scope.temporalStart = '';
+				$scope.temporalEnd = '';
+				$scope.notes = '';
+				if (!_.isEmpty($scope.pubData)) {
+					$scope.productDescription = $scope.pubData.productDescription;
+					$scope.numberOfPages = $scope.pubData.numberOfPages;
+					$scope.pageFirst = $scope.pubData.pageFirst;
+					$scope.pageLast = $scope.pubData.pageLast;
+					$scope.onlineOnly = $scope.pubData.onlineOnly;
+					$scope.additionalOnlineFiles = $scope.pubData.additionalOnlineFiles;
+					$scope.temporalStart = $scope.pubData.temporalStart;
+					$scope.temporalEnd = $scope.pubData.temporalEnd;
+					$scope.notes = $scope.pubData.notes;
+				}
+			}]);
+}) ();
