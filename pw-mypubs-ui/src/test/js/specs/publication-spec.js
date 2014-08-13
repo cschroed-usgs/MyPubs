3a9d9814327135f27dee032eae09e5cc6f010ed3
Carl Schroedl <cschroedl@usgs.gov>
2014-08-13 14:01:21 -0500

    Fixed asynch tests.

--- pw-mypubs-ui/src/test/js/specs/publication-spec.js
+++ pw-mypubs-ui/src/test/js/specs/publication-spec.js
@@ -1,3 +1,4 @@
+/* global angular*/
 describe("pw.publication module", function(){
 	var scope;
 
@@ -15,7 +16,7 @@ describe("pw.publication module", function(){
 	});
 
 	describe('publicationCtrl', function(){
-		var scope; var controller;
+		var scope, controller, $httpBackend, existingPub, newPub, newPubHandler, existingPubHandler;
 		beforeEach(function(){
 			//mock pubData
 			angular.module('pw.publication').constant('pubData' , {})
@@ -29,38 +30,82 @@ describe("pw.publication module", function(){
 				});
 				
 				}]);
+			inject(['Publication', '$httpBackend', 'PublicationPersister', function(Publication, _$httpBackend_, PublicationPersister){
+					newPub = new Publication();
+					existingPub = new Publication();
+					existingPub.id = 42;
+					$httpBackend = _$httpBackend_;
+					newPubHandler = $httpBackend.when('POST', PublicationPersister.PERSISTENCE_ENDPOINT);
+					newPubHandler.respond(newPub);
+					existingPubHandler = $httpBackend.when('PUT', PublicationPersister.PERSISTENCE_ENDPOINT + existingPub.id);
+					existingPubHandler.respond(existingPub);
+			}]);
 
+		});
+		afterEach(function() {
+			if($httpBackend){
+				$httpBackend.verifyNoOutstandingExpectation();
+				$httpBackend.verifyNoOutstandingRequest();
+			}
 		});
 		it('should have defined the tabs', function() {
 				expect(scope.tabs).toBeDefined();
 				expect( angular.isObject(scope.tabs) ).toBeTruthy();
 		});
-		it('should receive the persisted pubs object when it successfully persists the pub', function(done){
-			var existingPub, newPub;
-			inject(['Publication', function(Publication){
-					newPub = new Publication();
-					existingPub = new Publication();
-					existingPub.id = 42;
-			}]);
+		it('should receive the persisted pubs object when it successfully persists a new pub', function(done){
 			scope.pubData = newPub;
 			scope.$digest();
-			var persistPromise = scope.persistPub();
-			setTimeout(function(){
-				persistPromise.then(function(data){
+			var newPubPersistPromise = scope.persistPub();
+			newPubPersistPromise.then(function(data){
 				expect(data).toEqual(newPub);
-				done();
 			}, function(){
 				//this must fail if the function is called
 				expect(true).toBe(false);
-				done();
 			});
-			}, 0);
+			$httpBackend.flush();
+		});
+		it('should receive the persisted pubs object when it successfully persists an existing pub', function(){
+			scope.pubData = existingPub;
+			scope.$digest();
 			
-			
-			
+			var existingPubPersistPromise = scope.persistPub();
+			existingPubPersistPromise.then(function(data){
+				expect(data).toEqual(existingPub);
+			}, function(){
+				//this must fail if the function is called
+				expect(true).toBe(false);
+			});
+			$httpBackend.flush();
 		});
 		
-
+		it('should receive an error message when it unsuccessfully persists a new pub', function(){
+			newPubHandler.respond(404, '');
+			
+			scope.pubData = newPub;
+			scope.$digest();
+			var persistPromise = scope.persistPub();
+			persistPromise.then(function(data){
+				//this must fail if the function is called
+				expect(true).toBe(false);
+			}, function(message){
+				expect(message.length).not.toBe(0);
+			});
+			$httpBackend.flush();
+		});
+		it('should receive an error message when it unsuccessfully persists an existing pub', function(){
+			existingPubHandler.respond(404, '');
+			
+			scope.pubData = existingPub;
+			scope.$digest();
+			var persistPromise = scope.persistPub();
+			persistPromise.then(function(data){
+				//this must fail if the function is called
+				expect(true).toBe(false);
+			}, function(message){
+				expect(message.length).not.toBe(0);
+			});
+			$httpBackend.flush();
+		});
 	});
 
 	describe("Publication", function(){
