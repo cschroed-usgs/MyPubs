8f9b405b3eb72014eb801577852333d715f8c768
thongsav-usgs <thongsav@usgs.gov>
2014-08-19 16:47:15 -0500

    Hooked up global unauthorized handler and successfully forwarding to login page when needed.

--- pw-mypubs-ui/src/main/webapp/mypubs/auth/auth.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/auth/auth.js
@@ -1,12 +1,12 @@
 (function() {
 	var AUTH_SERVICE_PATH = 'auth/ad/token';
 	var LOGOUT_SERVICE_PATH = 'auth/ad/logout';
-	
+
 	angular.module('pw.auth', ['ngRoute'])
-	
+
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/Login', {
-			templateUrl: 'mypubs/main/login.html',
+			templateUrl: 'mypubs/auth/login.html',
 			controller: 'loginCtrl',
 			openAccess: true
 		});
@@ -16,32 +16,32 @@
 	 * This service is a stateful singleton and maintains a current logged in state
 	 */
 	.service('Authentication', ['$http','$location', '$q', '$cookies', function($http, $location, $q, $cookies) {
-		this.loginState = {
+		loginState = {
 				authToken : ''
 		};
 
 		this.getTokenPromise = function(user, pass) {
-			
+
 			//if we have a token stored in a browser cookie and none in memory, load from cookie
 			if(!this.loginState.authToken && $cookies.myPubsAuthToken) {
 				this.loginState.authToken = $cookies.myPubsAuthToken;
 			}
-			
+
 			//if no token exists, go to server
 			var _this = this;
 			if(!this.loginState.authToken) {
 				$http.post(APP_CONFIG.endpoint + AUTH_SERVICE_PATH,{
-	                params : {
-	                	username : user,
-	                	password : pass
-	                }
-	            }).success(function(response) {
-	            	$cookies.myPubsAuthToken = response;
-	            	_this.loginState.authToken = response;
-	            	deferred.resolve(_this.loginState.authToken);
-	            });
+					params : {
+						username : user,
+						password : pass
+					}
+				}).success(function(response) {
+					$cookies.myPubsAuthToken = response;
+					_this.loginState.authToken = response;
+					deferred.resolve(_this.loginState.authToken);
+				});
 			}
-			
+
 			var deferred = $q.defer(); //always return a promise
 			setTimeout(function() {
 				deferred.resolve(_this.loginState.authToken);
@@ -56,17 +56,33 @@
 		this.logout = function() {
 			var _this = this;
 			$http.get(APP_CONFIG.endpoint + LOGOUT_SERVICE_PATH,{
-                params : {
-                	token : _this.loginState.authToken || $cookies.myPubsAuthToken
-                }
-            }).success(function(response) {
-            	$location.path("/Login");
-            });
-			
-        	_this.loginState.authToken = null;
+				params : {
+					token : _this.loginState.authToken || $cookies.myPubsAuthToken
+				}
+			}).success(function(response) {
+				$location.path("/Login");
+			});
+
+			_this.loginState.authToken = null;
 			$cookies.myPubsAuthToken = null;
 		};
 
 		return this;
-	}]);
+	}])
+
+	.factory('UnauthorizedInterceptor', function($q, $location) {
+		var handleUnauthorized = function(response) {
+			
+			if(response.status == 401) {
+				$location.path("/Login");
+			} else {
+				return response;
+			}
+		};
+		return {
+			'response': handleUnauthorized,
+			'responseError': handleUnauthorized
+		};
+	});
+
 })();
