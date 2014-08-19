ddc54e1203c19f06fc0ab4c4d7a9bfb25a164537
thongsav-usgs <thongsav@usgs.gov>
2014-08-19 16:10:27 -0500

    Built out some auth functions and testing (not real world tested yet)

--- pw-mypubs-ui/src/main/webapp/mypubs/auth/auth.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/auth/auth.js
@@ -1,5 +1,7 @@
 (function() {
-
+	var AUTH_SERVICE_PATH = 'auth/ad/token';
+	var LOGOUT_SERVICE_PATH = 'auth/ad/logout';
+	
 	angular.module('pw.auth', ['ngRoute'])
 	
 	.config(['$routeProvider', function($routeProvider) {
@@ -7,31 +9,62 @@
 			templateUrl: 'mypubs/main/login.html',
 			controller: 'loginCtrl',
 			openAccess: true
-		})
+		});
 	}])
 
 	/**
 	 * This service is a stateful singleton and maintains a current logged in state
 	 */
-	.service('Authentication', ['$http','$location', function($http, $location) {
+	.service('Authentication', ['$http','$location', '$q', '$cookies', function($http, $location, $q, $cookies) {
 		this.loginState = {
-				user : '',
 				authToken : ''
 		};
 
-		this.getToken = function(token) {
+		this.getTokenPromise = function(user, pass) {
+			
+			//if we have a token stored in a browser cookie and none in memory, load from cookie
+			if(!this.loginState.authToken && $cookies.myPubsAuthToken) {
+				this.loginState.authToken = $cookies.myPubsAuthToken;
+			}
+			
+			//if no token exists, go to server
+			var _this = this;
+			if(!this.loginState.authToken) {
+				$http.post(APP_CONFIG.endpoint + AUTH_SERVICE_PATH,{
+	                params : {
+	                	username : user,
+	                	password : pass
+	                }
+	            }).success(function(response) {
+	            	$cookies.myPubsAuthToken = response;
+	            	_this.loginState.authToken = response;
+	            	deferred.resolve(_this.loginState.authToken);
+	            });
+			}
+			
+			var deferred = $q.defer(); //always return a promise
+			setTimeout(function() {
+				deferred.resolve(_this.loginState.authToken);
+			}, 10);
+
+			return deferred.promise;
 		};
 
 		/**
 		 * Invalidates token on service, in cookie, and in memory; then routes to login page
 		 */
 		this.logout = function() {
-		};
-
-		/**
-		 * provides method to validate token with auth service
-		 */
-		auth.validateCurrentToken = function() {
+			var _this = this;
+			$http.get(APP_CONFIG.endpoint + LOGOUT_SERVICE_PATH,{
+                params : {
+                	token : _this.loginState.authToken || $cookies.myPubsAuthToken
+                }
+            }).success(function(response) {
+            	$location.path("/Login");
+            });
+			
+        	_this.loginState.authToken = null;
+			$cookies.myPubsAuthToken = null;
 		};
 
 		return this;
--- pw-mypubs-ui/src/test/js/specs/auth-spec.js
+++ pw-mypubs-ui/src/test/js/specs/auth-spec.js
@@ -1,23 +1,116 @@
 describe("pw.auth module", function() {
-	var scope, location;
-
-
-	beforeEach(function () {
-		module('pw.auth');
-		inject(function($rootScope, $location) {
-		});
-	});
-
+	//set global constant
+	APP_CONFIG = {
+			endpoint: 'http://servicesUrl.com/pubs-service/'
+	};
+	var AUTH_SERVICE_PATH = 'auth/ad/token';
+	var LOGOUT_SERVICE_PATH = 'auth/ad/logout';
+	
+	var AD_TOKEN_URL = APP_CONFIG.endpoint + AUTH_SERVICE_PATH;
+	var LOGOUT_URL = APP_CONFIG.endpoint + LOGOUT_SERVICE_PATH;
+	
 	describe("Authentication service", function() {
+		var authenticationService, httpBackend, locationMock, cookiesMock;
+
+		beforeEach(function () {
+			locationMock = {
+					path : function(newPath) {}
+			};
+			spyOn(locationMock, 'path').andCallThrough();
+
+			cookiesMock = {};
+
+			module('pw.auth', function($provide) {
+				// location mock
+				$provide.value('$location', locationMock);
+				$provide.value('$cookies', cookiesMock);
+			});
+
+			inject(function(Authentication, $httpBackend) {
+				authenticationService = Authentication;
+				httpBackend = $httpBackend;
+			});
+
+			//no http calls pending when we first start
+			httpBackend.verifyNoOutstandingExpectation();
+			httpBackend.verifyNoOutstandingRequest();
+		});
+
+		afterEach(function() {
+			httpBackend.verifyNoOutstandingRequest();
+		});
+
+
 		it("contains these API functions", function (){
+			expect(authenticationService.getTokenPromise).toBeDefined();
+			expect(authenticationService.logout).toBeDefined();
+		});
+
+		it("getTokenPromise will do an ajax call to a auth service to get a token " +
+				"when no token exists in javascript memory or browser cookes", function (){
+			//set up server responses
+			var testToken = "auth-token";
+			httpBackend.whenPOST(AD_TOKEN_URL).respond(testToken);
+
+			//first call results in a fetch to token server
+			authenticationService.getTokenPromise("user", "pass").then(function(token) {
+				expect(token).toBe(testToken);
+				expect(cookiesMock.myPubsAuthToken).toBe(testToken);
+			});
+			httpBackend.expectPOST(AD_TOKEN_URL);
+			httpBackend.flush();
 		});
 		
-		it("returns a promise for a token that is either 1) in memory (loginState), " +
-				"2) in a browser cookie (and subsequently stored in loginState), or " +
-				"3 fetched from auth token service using a post (and subsequently stored in loginState)", function (){});
+		it("getTokenPromise get the token from a browser cookie if it exists " +
+				"when no token exists in javascript memory or browser cookes", function (){
+			//set up server responses
+			var testToken = "auth-token";
+			var testCookieToken = "token-from-cookie";
+			httpBackend.whenPOST(AD_TOKEN_URL).respond(testToken);
+
+			cookiesMock.myPubsAuthToken = testCookieToken; //preset the cookie
+			
+			authenticationService.getTokenPromise("user", "pass").then(function(token) {
+				expect(token).toBe(testCookieToken);
+				expect(cookiesMock.myPubsAuthToken).toBe(testCookieToken);
+			});
+		});
 		
-		it("calls a token invalidation service, clears token from cookie, and in memory; then routes to login page", function (){});
-		
-		it("provides method to validate token with an auth service", function (){});
+		it("getTokenPromise get the token from javascript memory if there", function (){
+			//set up server responses
+			var testToken = "auth-token";
+			var testCookieToken = "token-from-cookie";
+			var jsMemoryCookieToken = "token-from-memory";
+			httpBackend.whenPOST(AD_TOKEN_URL).respond(testToken);
+
+			authenticationService.loginState.authToken = jsMemoryCookieToken;
+			cookiesMock.myPubsAuthToken = null; //no cookie
+			
+			authenticationService.getTokenPromise("user", "pass").then(function(token) {
+				expect(token).toBe(jsMemoryCookieToken);
+				expect(cookiesMock.myPubsAuthToken).toBe(jsMemoryCookieToken);
+			});
+		});
+
+		it("logout calls a token invalidation service, clears token from cookie, and in memory; then routes to login page", function (){
+			//first log in
+			var testToken = "auth-token";
+			httpBackend.whenPOST(AD_TOKEN_URL).respond(testToken);
+			authenticationService.getTokenPromise("user", "pass").then(function(token) {
+				expect(token).toBe(testToken);
+				expect(cookiesMock.myPubsAuthToken).toBe(testToken);
+			});
+			httpBackend.expectPOST(AD_TOKEN_URL);
+			httpBackend.flush();
+			
+			httpBackend.whenGET(LOGOUT_URL + "?token=" + testToken).respond("Logged out"); //TODO check for 200?
+			authenticationService.logout();
+			httpBackend.expectGET(LOGOUT_URL + "?token=" + testToken);
+			httpBackend.flush();
+			
+			expect(cookiesMock.myPubsAuthToken).toBeFalsy();
+			expect(authenticationService.loginState.authToken).toBeFalsy();
+			expect(locationMock.path).toHaveBeenCalledWith('/Login');
+		});
 	});
 });
